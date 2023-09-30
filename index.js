d9fb457ebd63e3354cb0eba1c882c9ff3eaad38c
kazibilsi <kazibilasi@gmail.com>
2023-09-30 22:42:55 +0600

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require("jsonwebtoken")
 const app = express()
 const port = process.env.PORT || 5000
 // online-learning-platform
@@ -8,6 +9,21 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: ' unauthorized access' })
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: ' unauthorized access' })
+    }
+    req.decoded = decoded
+    next();
+  })
+}
 
 
 
@@ -31,6 +47,34 @@ async function run() {
     const discussionCollection = client.db('online-learning-platform').collection('discussion')
     const usersCollection = client.db('online-learning-platform').collection('users')
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+      res.send({ token })
+    })
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query)
+      if (user?.role !== 'admin') {
+        return res.send({ admin: false })
+      }
+      next()
+    }
+
+    // const verifyInstructor = async (req, res, next) => {
+    //   const email = req.decoded.email
+
+    //   const query = {  : email }
+    //   const user = await courseCollection.findOne(query)
+    //   if (user?.role !== 'admin') {
+    //     return res.send({ admin: false })
+    //   }
+    //   next()
+    // }
+
     app.get('/courses', async(req,res)=>{
         const result = await courseCollection.find().toArray()
         res.send(result);
@@ -52,12 +96,73 @@ async function run() {
         const comment = await discussionCollection.find().toArray()
         res.send(comment)
     })
-    app.post('/users', async(req, res)=>{
-        const users = req.body
-        const result = await usersCollection.insertOne(users)
+    app.get('/users', async(req, res)=>{
+        
+        const result = await usersCollection.find().toArray()
         res.send(result)
     })
+    app.post('/users', async (req, res) => {
+      const users = req.body;
+      const query = { email: users.email }
+      const existingUser = await usersCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: " user already exist" })
+      }
+      const result = await usersCollection.insertOne(users)
+      res.send(result)
+    })
 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        res.send({instructor:false})
+      }
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" }
+      res.send(result);
+    })
+
+
+   
+
+    app.get('/users/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+      const query = { email: email }
+      const user = await usersCollection.findOne(query)
+
+      const result = { admin: user?.role === 'admin' }
+
+      res.send(result)
+    })
 
 
     await client.connect();
